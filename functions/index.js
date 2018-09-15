03e208315f3bb98aa9e9c20e43755dc65abcbafa
kath2377 <kjav2377@gmail.com>
2018-09-15 08:31:00 -0500

    First commit
    Two functions were created.
    One for saving in firebase
    The second one for retrieving data from firebase

---
+++ functions/index.js
@@ -0,0 +1,67 @@
+'use strict';
+const functions = require('firebase-functions');
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
+const firebaseAdmin = require('firebase-admin');
+const { WebhookClient } = require('dialogflow-fulfillment');
+const { Card, Suggestion } = require('dialogflow-fulfillment');
+firebaseAdmin.initializeApp(functions.config().firebase);
+
+exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
+    const agent = new WebhookClient({ request, response });
+    console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
+    console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
+
+    function saveName(agent) {
+        const nameParam = agent.parameters.name;
+        console.log('name 1: ' + nameParam);
+        console.log('name 2: ' + request.body.queryResult.parameters.name);
+        return firebaseAdmin.firestore().collection('users').where('name', '==', nameParam).limit(1).get()
+            .then(snapshot => {
+                let user = snapshot.docs[0];
+                if (!user) {
+                    console.log('Adding new user');
+                    agent.add('Bienvenida ' + nameParam);
+                    firebaseAdmin.firestore().collection('users').add({
+                        name: nameParam
+                    });
+                }
+                else {
+                    console.log('User already exists');
+                    agent.add('Bienvenida de nuevo ' + nameParam);
+                }
+                return Promise.resolve('done');
+            }).catch(error => console.log('ERROR: ' + error));
+    }
+
+
+    function sendVideo(agent){
+        return firebaseAdmin.firestore().collection('learningObjects').where('type','==','video').limit(1).get()
+        .then(snapshot => {
+            snapshot.forEach((video) => {
+            console.log('video id: '+video.id);
+            console.log('video name: '+video.data().name);
+            agent.add(new Card({
+                title: video.data().name,
+                imageUrl: 'https://www.nocreasnada.com/wp-content/uploads/2018/08/2018-08-27_5b8418533078a_Using-Videos-Affiliate-Marketing.jpg',
+                text: '¡Mira este video!',
+                buttonText: 'Ver video',
+                buttonUrl: video.data().url
+            }));
+            agent.add('¡Mira! Encontré este video para ti');
+            agent.add(new Suggestion('Gracias'));
+            })
+            return Promise.resolve('done');
+        }).catch(error => console.log('ERROR: ' + error));
+    }
+
+    let intentMap = new Map();
+    intentMap.set('SaveMyName', saveName);
+    intentMap.set('WatchVideo', sendVideo);
+    return agent.handleRequest(intentMap);
+});
