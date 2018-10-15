82598a6b519d0ed340f95eb0492d624002ec82ad
kath2377 <kjav2377@gmail.com>
2018-10-15 07:24:28 -0500

    funcion solveConceptQuestion

--- functions/index.js
+++ functions/index.js
@@ -40,32 +40,40 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
     }
 
 
-    function sendVideo(agent){
-        return firebaseAdmin.firestore().collection('learningObjects').where('type','==','video').limit(1).get()
-        .then(snapshot => {
-            snapshot.forEach((video) => {
-            console.log('video id: '+video.id);
-            console.log('video name: '+video.data().name);
-            agent.add(new Card({
-                title: video.data().name,
-                imageUrl: 'https://www.nocreasnada.com/wp-content/uploads/2018/08/2018-08-27_5b8418533078a_Using-Videos-Affiliate-Marketing.jpg',
-                text: '¡Mira este video!',
-                buttonText: 'Ver video',
-                buttonUrl: video.data().url
-            }));
-            agent.add('¡Mira! Encontré este video para ti');
-            agent.add(new Suggestion('Gracias'));
-            })
-            return Promise.resolve('done');
-        }).catch(error => console.log('ERROR - sendVideo: ' + error));
+    function sendVideo(agent) {
+        return firebaseAdmin.firestore().collection('learningObjects').where('type', '==', 'video').limit(1).get()
+            .then(snapshot => {
+                snapshot.forEach((video) => {
+                    console.log('video id: ' + video.id);
+                    console.log('video name: ' + video.data().name);
+                    agent.add(new Card({
+                        title: video.data().name,
+                        imageUrl: 'https://www.nocreasnada.com/wp-content/uploads/2018/08/2018-08-27_5b8418533078a_Using-Videos-Affiliate-Marketing.jpg',
+                        text: '¡Mira este video!',
+                        buttonText: 'Ver video',
+                        buttonUrl: video.data().url
+                    }));
+                    agent.add('¡Mira! Encontré este video para ti');
+                    agent.add(new Suggestion('Gracias'));
+                })
+                return Promise.resolve('done');
+            }).catch(error => console.log('ERROR - sendVideo: ' + error));
     }
 
-    function solveConceptQuestion(agent){
+    function solveConceptQuestion(agent) {
         const conceptName = agent.parameters.conceptName;
-        firebaseAdmin.firestore().collection('learningUnits/{unitId}/topics').where('keywords','array-contains',conceptName).limit(1).get()
-        .then(snapshot => {
-            console.log('topic name = '+snapshot[0].data().name);
-        }).catch(error => console.log('ERROR - solveConceptQuestion: ' + error));
+        console.log('conceptName = ' + conceptName);
+        return firebaseAdmin.firestore().collection('learningUnits/{unitId}/topics').where('keywords', 'array-contains', conceptName).limit(1).get()
+            .then(snapshot => {
+                const topic = snapshot.docs[0];
+                if(!topic){
+                    console.log('No topic found');
+                }
+                else{
+                    console.log('topic name = ' + topic.data().name);
+                }
+                return Promise.resolve('done');
+            }).catch(error => console.log('ERROR - solveConceptQuestion: ' + error));
 
     }
 
