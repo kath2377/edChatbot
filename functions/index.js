97d14502c382718e83538cc1c1170a136d401cd1
kapazave <kapazave@everis.com>
2018-10-15 15:18:54 -0500

    finished show learning object to solve concept question

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,6 @@
 'use strict';
 const functions = require('firebase-functions');
+const videoPlayerThumbnail = 'https://www.nocreasnada.com/wp-content/uploads/2018/08/2018-08-27_5b8418533078a_Using-Videos-Affiliate-Marketing.jpg';
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -48,7 +49,7 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
                     console.log('video name: ' + video.data().name);
                     agent.add(new Card({
                         title: video.data().name,
-                        imageUrl: 'https://www.nocreasnada.com/wp-content/uploads/2018/08/2018-08-27_5b8418533078a_Using-Videos-Affiliate-Marketing.jpg',
+                        imageUrl: videoPlayerThumbnail,
                         text: '¡Mira este video!',
                         buttonText: 'Ver video',
                         buttonUrl: video.data().url
@@ -62,19 +63,54 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
 
     function solveConceptQuestion(agent) {
         const conceptName = agent.parameters.conceptName;
-        console.log('conceptName = ' + conceptName);
-        return firebaseAdmin.firestore().collection('learningUnits/{unitId}/topics').where('keywords', 'array-contains', conceptName).limit(1).get()
+        console.log('conceptNameee = ' + conceptName);
+        return firebaseAdmin.firestore().collection('learningTopics').where('keywords', 'array-contains', conceptName).limit(1).get()
             .then(snapshot => {
                 const topic = snapshot.docs[0];
-                if(!topic){
+                if (!topic) {
                     console.log('No topic found');
+                    return Promise.resolve('done');
                 }
-                else{
+                else {
                     console.log('topic name = ' + topic.data().name);
+                    console.log('topic id = ' + topic.id);
+                    return sendLearningObject(agent, topic.id);
                 }
-                return Promise.resolve('done');
             }).catch(error => console.log('ERROR - solveConceptQuestion: ' + error));
 
+    }
+
+    //options not related to Intents
+    function sendLearningObject(agent, topicId) {
+        return firebaseAdmin.firestore().collection('learningObjects').where('topicId', '==', topicId).limit(1).get()
+            .then(snapshot => {
+                const learningObject = snapshot.docs[0];
+                if (!learningObject) {
+                    console.log('No learningObject found');
+                    return Promise.resolve('done');
+                }
+                else {
+                    console.log('object name = ' + learningObject.data().name);
+                    return showLearningObject(agent, learningObject).catch(error => console.log('ERROR - showLearningObject: ' + error));
+                }
+            }).catch(error => console.log('ERROR - sendLearningObject: ' + error));
+    }
+
+    function showLearningObject(agent, learningObject) {
+        console.log('object type: '+learningObject.data().type);
+        if (learningObject.data().type === 'image' || learningObject.data().type === 'video') {
+            console.log('it is an image or a video');
+            agent.add(new Card({
+                title: learningObject.data().name,
+                imageUrl: learningObject.data().type === 'image' ? learningObject.data().url : videoPlayerThumbnail,
+                text: learningObject.data().description,
+                buttonText: 'Ver',
+                buttonUrl: learningObject.data().url
+            }));
+            agent.add('¡Mira! Puedes revisar este material sobre el tema');
+        }
+        console.log('the end');
+        return Promise.resolve('done');
     }
 
     let intentMap = new Map();
